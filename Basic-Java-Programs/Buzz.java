a3ce1e63f8dd4b2fa302835ffa86e6c296949901
Arpita Panda <162110210+Arpita-023p@users.noreply.github.com>
2026-07-18 22:35:06 +0530

    check whether a number is a buzz number or not

---
+++ Basic-Java-Programs/Buzz.java
@@ -0,0 +1,15 @@
+public class Buzz {
+    public static void main(String[] args) {
+        int number=49;
+        int rem;
+        int digit;
+         rem=number%10;
+         digit=number%7;
+        if(digit==0||rem==7){
+         System.out.println("buzz");
+        }
+        else{
+        System.out.println("not buzz");
+        }
+    }
+}
