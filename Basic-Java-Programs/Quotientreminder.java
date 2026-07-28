185da31442151b371fbdca7b3c01bbaf37f374a8
Arpita Panda <arpitapanda027@gmail.com>
2026-07-28 23:20:10 +0530

    Save local progress before pull

---
+++ Basic-Java-Programs/Quotientreminder.java
@@ -0,0 +1,9 @@
+public class Quotientreminder {
+    public static void main(String[] args) {
+        int num=123;
+        int reminder=num%10;
+        int quotient=num/10;
+        System.out.println("the reminder is "+reminder);
+        System.out.println("the quotient is "+quotient);
+    }
+}
