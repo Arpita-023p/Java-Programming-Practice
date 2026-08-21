e29082d3da25b3d25156431c2318a171fef30d41
Arpita Panda <162110210+Arpita-023p@users.noreply.github.com>
2026-08-21 10:42:40 +0530

    printing the array elemet

---
+++ Array/Arrayprint.java
@@ -0,0 +1,8 @@
+public class Arrayprint {
+    public static void main(String[] args) {
+        int arr[]={10,45,67,89,47};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
