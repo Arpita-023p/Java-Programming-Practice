2f0d98e16e3817d56faccf1b408edeef3879ce00
Arpita Panda <162110210+Arpita-023p@users.noreply.github.com>
2026-08-21 10:46:07 +0530

    Negative number

---
+++ Array/Negativearr.java
@@ -0,0 +1,10 @@
+public class Negativearr {
+     public static void main(String[] args) {
+      int arr[]={0,45,-89,-32,47};  
+      for(int i=0;i<arr.length;i++){
+        if(arr[i]<0){
+        System.out.println(arr[i]);
+        }
+      }
+    }
+}
